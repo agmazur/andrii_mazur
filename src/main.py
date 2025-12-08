262fd6917b123489b5849b3d70ab3cd8fb5aa288
Andrii Mazur <agmazur888@gmail.com>
2025-12-08 22:11:53 +0100

    added basik word separation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,16 @@
-import 
+from scrape_website import scrape_website
+from text_extraction_from_html import text_extractoin_from_html
+
+
+
+
+
+
+scraped =scrape_website("https://www.hslu.ch/de-ch/")
+extracted_text=text_extractoin_from_html(scraped)
+for element in extracted_text:
+    print(f"{element}+")
+
 
 import time
 time.sleep(10)
---
+++ src/text_extraction_from_html.py
@@ -0,0 +1,15 @@
+def text_extractoin_from_html(html):
+    from bs4 import BeautifulSoup
+    soup = BeautifulSoup(html, "html.parser")
+    all_p_tags=soup.find_all('p')
+    p_text=[]
+    for ptag in all_p_tags:
+        if ptag.get_text()=="":
+            pass
+        else:
+            p_text.append(ptag.get_text())
+
+
+
+    print(p_text)
+    return p_text
