f4b63932db02b92723a1d64a41bfc89d53935c09
Andrii Mazur <agmazur888@gmail.com>
2025-12-09 12:06:34 +0100

    added data cleaning

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ from text_extraction_from_html import text_extractoin_from_html
 from basik_sentiment_analysis import basik_sentiment_analysis
 from sqlite_interactions import putdata_to_databank ,retrieve_data_by_website,fetch_different_websites
 from data_visualization import creaet_graphic
+from regex_cleaning import clean_array_strings
 
 
 websitelink="https://www.tagesschau.de"
 scraped_html =scrape_website(websitelink)
 extracted_text=text_extractoin_from_html(scraped_html)
-sentient_p_n,sentient_value=basik_sentiment_analysis(extracted_text)
-putdata_to_databank(websitelink,extracted_text,sentient_p_n,sentient_value)
+better_text=clean_array_strings(extracted_text)
+sentient_p_n,sentient_value=basik_sentiment_analysis(better_text)
+putdata_to_databank(websitelink,better_text,sentient_p_n,sentient_value)
 # fetched_df=retrieve_data_by_website(websitelink)
 # my_plt=creaet_graphic(fetched_df)
 # my_plt.show()
--- src/regex_cleaning.py
+++ src/regex_cleaning.py
@@ -6,11 +6,19 @@ def clean_array_strings(array_of_strings):
     array1=[]
     pattern = re.compile(r'[\t\n]+')
     for s in array_of_strings:
-        # 2. Use re.sub to replace the targeted characters with an empty string ('')
         cleaned_string = pattern.sub('', s)
-        # 3. Strip any remaining leading or trailing spaces (which were NOT removed by the regex)
-        # 4. Filter: Only include the string if it is not empty after cleaning
         if cleaned_string:
             array1.append(cleaned_string)
-        rejoined_string = "".join(array1)
-    return rejoined_string.split()
+        rejoined_string = "".join(array1).split()
+    sentences = []
+    current_sentence_words = []
+    sentence_end_pattern = re.compile(r'[.!?]$')
+    for word in rejoined_string:
+        current_sentence_words.append(word)
+        if sentence_end_pattern.search(word):
+            sentence = " ".join(current_sentence_words)
+            sentences.append(sentence)
+            current_sentence_words = [] # Reset for the next sentence
+    if current_sentence_words:
+        sentences.append(" ".join(current_sentence_words))
+    return sentences    
